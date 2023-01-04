e8f85da8655d0ed1150d16dae5e166a28fa1c1e5
johnszyna <jan.szyndlarewicz@gmail.com>
2023-01-04 12:06:48 +0100

    Adding many checkAlias to one object

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -28,6 +28,7 @@ public class Main {
         }
     }
 
+
     public static void main(String[] args) {
 
 
@@ -45,5 +46,7 @@ public class Main {
 
         //printDeviceObserver(home, 0);
         printDeviceObservers(home);
+
+
     }
 }
