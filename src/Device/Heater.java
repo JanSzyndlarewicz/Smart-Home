b56f4a742303ce632b3ffb109c107628470f3c4e
Wiggy719 <igor.wojtun@gmail.com>
2023-01-11 13:21:16 +0100

    test

--- src/Device/Heater.java
+++ src/Device/Heater.java
@@ -19,5 +19,5 @@ public class Heater extends Device {
 		addProperty(new DevicePropertySlider("Temperature", 0));
 		addProperty(new DevicePropertyToggle("Is turned on", false));
 	}
-//test
+//tests
 }
