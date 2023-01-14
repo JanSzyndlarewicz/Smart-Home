78303b8fe57134391114c1ca88b9d4e3ef5eb6b6
Dariusz Szypka <darkoshypka@gmail.com>
2023-01-14 21:13:18 +0100

    Added DeviceProperties very simple GUI. Not fully optimized

---
+++ src/View/RunShowDevicePropertiesFrame.java
@@ -0,0 +1,15 @@
+package View;
+
+public class RunShowDevicePropertiesFrame {
+
+    public static void main(String[] args) {
+
+        ShowDevicePropertiesFrame devicePropertiesGui = new ShowDevicePropertiesFrame();
+
+
+        }
+
+    }
+
+
+
