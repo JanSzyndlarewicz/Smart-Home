b03f715b7eeaac6a5555610c1b45568e76c5af79
Kacper Tomczyk <kacperus.tomczyk@gmail.com>
2023-01-21 18:05:57 +0100

    added min and max values for some outputDevices

--- src/Device/Heater.java
+++ src/Device/Heater.java
@@ -5,17 +5,17 @@ import DeviceProperty.DevicePropertyToggle;
 public class Heater extends OutputDevice {
 	public Heater() {
 		super();
-		addProperty(new DevicePropertySlider("Temperature", 0));
+		addProperty(new DevicePropertySlider("Temperature", 0, -100, 100));
 		addProperty(new DevicePropertyToggle("Is turned on", false));
 	}
 	public Heater(String alias) {
 		super(alias);
-		addProperty(new DevicePropertySlider("Temperature", 0));
+		addProperty(new DevicePropertySlider("Temperature", 0, -100, 100));
 		addProperty(new DevicePropertyToggle("Is turned on", false));
 	}
 	public Heater(String alias, String location) {
 		super(alias, location);
-		addProperty(new DevicePropertySlider("Temperature", 0));
+		addProperty(new DevicePropertySlider("Temperature", 0, -100, 100));
 		addProperty(new DevicePropertyToggle("Is turned on", false));
 	}
 	
--- src/Device/Shutter.java
+++ src/Device/Shutter.java
@@ -4,25 +4,25 @@ import DeviceProperty.DevicePropertySlider;
 public class Shutter extends OutputDevice {
 	public Shutter() {
 		super();
-		addProperty(new DevicePropertySlider("Length", 0));
-		addProperty(new DevicePropertySlider("Angle", 0));
-		addProperty(new DevicePropertySlider("Speed", 0));
+		addProperty(new DevicePropertySlider("Length", 0, 0, 100));
+		addProperty(new DevicePropertySlider("Angle", 0, 0, 90));
+		addProperty(new DevicePropertySlider("Speed", 0, 0, 100));
 		
 	}
 	public Shutter(String alias) {
 		super(alias);
 
-		addProperty(new DevicePropertySlider("Length", 0));
-		addProperty(new DevicePropertySlider("Angle", 0));
-		addProperty(new DevicePropertySlider("Speed", 0));
+		addProperty(new DevicePropertySlider("Length", 0, 0, 100));
+		addProperty(new DevicePropertySlider("Angle", 0, 0, 90));
+		addProperty(new DevicePropertySlider("Speed", 0, 0, 100));
 
 	}
 	public Shutter(String alias,String location) {
 		super(alias);
 
-		addProperty(new DevicePropertySlider("Length", 0));
-		addProperty(new DevicePropertySlider("Angle", 0));
-		addProperty(new DevicePropertySlider("Speed", 0));
+		addProperty(new DevicePropertySlider("Length", 0, 0, 100));
+		addProperty(new DevicePropertySlider("Angle", 0, 0, 90));
+		addProperty(new DevicePropertySlider("Speed", 0, 0, 100));
 
 	}
 
