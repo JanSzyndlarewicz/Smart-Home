b390f3cb730a4fe2d66b2b742e25ce4e1f38a4cb
johnszyna <jan.szyndlarewicz@gmail.com>
2023-01-22 20:10:00 +0100

    StartFrame

--- src/View/StartFrame.java
+++ src/View/StartFrame.java
@@ -6,41 +6,72 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class StartFrame extends JFrame {
+
     private JPanel panel;
-    private JLabel loginLabel;
-    private JLabel registerLabel;
     private JButton loginButton;
     private JButton registerButton;
+    private JLabel welcomeMess;
+
+    private JPanel designPanel1;
+    private JPanel designPanel2;
+    private JPanel designPanel3;
+    private JPanel designPanel4;
+    private Color designPanelColor;
 
     public StartFrame(){
         initialize();
-        setComponents();
-        setVisible(true);
     }
 
     public void initialize(){
         panel = new JPanel();
-        loginButton = new JButton();
-        registerButton = new JButton();
+        welcomeMess = new JLabel("Choose an option");
+        loginButton = new JButton("Sign in");
 
-    }
+        registerButton = new JButton("Sign up");
+        designPanel1 = new JPanel();
+        designPanel2 = new JPanel();
+        designPanel3 = new JPanel();
+        designPanel4 = new JPanel();
+        designPanelColor = new Color(123, 3, 252);
 
-    public void setComponents(){
-        loginButton.setText("Login");
-        registerButton.setText("Register");
-        loginButton.setBounds(40, 100, 150, 90);
-        registerButton.setBounds(40, 250, 150, 90);
         panel.setLayout(null);
         panel.setBackground(new Color(223, 237, 245));
-        panel.add(registerButton);
-        panel.add(loginButton);
-        add(panel);
-        registerButton.addActionListener(new GoToRegisterFrame());
+
+        designPanel1.setBackground(designPanelColor);
+        designPanel2.setBackground(designPanelColor);
+        designPanel3.setBackground(designPanelColor);
+        designPanel4.setBackground(designPanelColor);
+        welcomeMess.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 17));
+
+        // placement
+        designPanel1.setBounds(360, 0, 125, 500);
+        designPanel2.setBounds(0, 0, 125, 500);
+        designPanel3.setBounds(0, 0, 600, 10);
+        designPanel4.setBounds(0, 500, 600, 10);
+        welcomeMess.setBounds(170, 50, 200, 50);
+        loginButton.setBounds(190, 200, 100, 35);
+        registerButton.setBounds(190, 270, 100, 35);
+
         loginButton.addActionListener(new GoToLoginFrame());
-        setSize(250, 400);
+        registerButton.addActionListener(new GoToRegisterFrame());
+
+        panel.add(designPanel1);
+        panel.add(designPanel2);
+        panel.add(designPanel3);
+        panel.add(designPanel4);
+        panel.add(welcomeMess);
+        panel.add(loginButton);
+        panel.add(registerButton);
+
+        add(panel);
+
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
+        setVisible(true);
+
     }
+
 
     private class GoToRegisterFrame implements ActionListener {
 
